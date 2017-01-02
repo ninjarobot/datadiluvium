e95bf28b7e87cad82050a43fb54882aa903069ac
Adron Hall <adronhall@gmail.com>
2017-01-01 17:51:02 -0800

    The latest hugo site publishing, now @ $( date +%m-%d-%y-%H%M ).

--- docs/hugo/push-latest.sh
+++ docs/hugo/push-latest.sh
@@ -3,22 +3,26 @@ timestamp=$( date +%m-%d-%y-%H%M )
 echo $timestamp
 filename="datadiluvium-$timestamp.tgz"
 
-echo "Generating Hugo Site with landing-page-hugo and cleaning up .DS_Store cruft."
+echo "Generating Hugo Site with landing-page-hugo and cleaning up .DS_Store cruft.\n * * * * *"
 hugo --theme=landing-page-hugo
 find . -name '.DS_Store' -type f -delete
-echo "Copying latest published content."
+echo "Copying latest published content.\n * * * * *"
 tar cvfz $filename public/
-echo "Moving content to docs directory."
+echo "Moving content to docs directory.\n * * * * *"
 cp $filename ../datadiluvium.tgz
 
 cd ..
 tar -xf datadiluvium.tgz
 rm -rf datadiluvium.tgz
 cd ..
+echo "Removing old site.\n * * * * *"
+rm -rf about css font-awesome-4.1.0 fonts img index.html index.xml js services sitemap.xml
+echo "Placing new site.\n * * * * *"
 mv -v docs/public/* docs
 cd docs
 rm -rf public
 cd ../
+echo "Committing and pushing new site.\n * * * * *"
 git add -A
 git commit -m 'The latest hugo site publishing, now @ $( date +%m-%d-%y-%H%M ).'
 git push origin master
