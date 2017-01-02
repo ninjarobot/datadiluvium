e821895546be6a86a7ab8f403a7df6dcf9fccbeb
Adron Hall <adronhall@gmail.com>
2017-01-01 17:43:43 -0800

    Add publish script and created first rough draft of site.

--- docs/hugo/push-latest.sh
+++ docs/hugo/push-latest.sh
@@ -1,7 +1,20 @@
-echo "Generating Hugo Site with landing-page-hugo."
+# timestamp=`date mm-dd-yy-HHMM`
+timestamp=$( date +%m-%d-%y-%H%M )
+echo $timestamp
+filename="datadiluvium-$timestamp.tgz"
+
+echo "Generating Hugo Site with landing-page-hugo and cleaning up .DS_Store cruft."
 hugo --theme=landing-page-hugo
+find . -name '.DS_Store' -type f -delete
 echo "Copying latest published content."
-tar cvfz datadiluvium.tgz public/*
-mv datadiluvium.tgz ../datadiluvium.tgz
+tar cvfz $filename public/
+echo "Moving content to docs directory."
+cp $filename ../datadiluvium.tgz
+
 cd ..
 tar -xf datadiluvium.tgz
+rm -rf datadiluvium.tgz
+cd ..
+mv -v docs/public/* docs
+cd docs
+rm -rf public
