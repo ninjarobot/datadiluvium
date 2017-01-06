ca8d9f163d7b930e89cd414f162c48c93b0b0e0e
Adron Hall <adronhall@gmail.com>
2017-01-06 14:25:19 -0800

    Adding a sample fake data generation library for use.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
     "fmt"
     "io/ioutil"
     "os"
+    "github.com/icrowley/fake"
+    "time"
 )
 
 type Page struct {
@@ -36,6 +38,63 @@ func main() {
     }
 
     fmt.Println(toJson(pages))
+
+    fmt.Println("\nSome Russian Random Data")
+    fake.SetLang("ru")
+    fmt.Println(fake.FirstName())
+    fmt.Println(fake.FullName())
+    fmt.Println(fake.Product())
+    fmt.Println(fake.Brand())
+    fmt.Println(fake.Character())
+    fmt.Println(fake.City())
+    fmt.Println(fake.Color())
+    fmt.Println(fake.Company())
+    fmt.Println(fake.Continent())
+    fmt.Println(fake.Country())
+    fmt.Println(fake.Day())
+    fmt.Println(fake.CurrencyCode())
+    fmt.Println(fake.Currency())
+    fmt.Println(fake.Currency())
+
+    fmt.Println("\n\n\nSome English Random Data")
+    fake.SetLang("en")
+    fmt.Println(fake.FirstName())
+    fmt.Println(fake.FullName())
+    fmt.Println(fake.Product())
+    fmt.Println(fake.Brand())
+    fmt.Println(fake.Character())
+    fmt.Println(fake.City())
+    fmt.Println(fake.Color())
+    fmt.Println(fake.Company())
+    fmt.Println(fake.Continent())
+    fmt.Println(fake.Country())
+    fmt.Println(fake.Day())
+    fmt.Println(fake.CurrencyCode())
+    fmt.Println(fake.Currency())
+    fmt.Println(fake.Currency())
+
+    thisMany := 10
+    counter := 1
+    t := time.Now().Format(time.RFC850)
+
+    fmt.Println("\n\n\nPrinting", thisMany)
+
+  	for i := 0; i < thisMany; i++ {
+      fmt.Println(counter, fake.FirstName())
+      counter += 1
+      fake.FirstName()
+  	}
+
+    // ~2 sec  Running 10 Million Name Generations
+    // Start Time:  Friday, 06-Jan-17 14:16:39 PST
+    // End Time:  Friday, 06-Jan-17 14:16:41 PST
+    // ~13 sec Running with fmt.Println + counter
+    // Start Time:  Friday, 06-Jan-17 14:14:46 PST
+    // End Time:  Friday, 06-Jan-17 14:15:59 PST
+
+    fmt.Println("Start Time: ", t)
+    t = time.Now().Format(time.RFC850)
+    fmt.Println("End Time: ", t)
 }
 
 func getPages() []Page {
