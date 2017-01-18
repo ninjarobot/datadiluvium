c9bf69e9b6bda956600c83aee4f941b97c45d70f
Adron Hall <adronhall@gmail.com>
2017-01-17 16:35:38 -0800

    Added comments to the shell.

--- setup.sh
+++ setup.sh
@@ -12,16 +12,17 @@ if [ "$(uname)" == "Darwin" ]; then
         brew update
         brew install glide
     fi
-
     echo "Installing jet cli."
     brew cask install jet
-
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Installing Glide for Linux."
+    # This needs testing... I'll be doing that later today.
     sudo add-apt-repository ppa:masterminds/glide && sudo apt-get update
     sudo apt-get install glide
+    # Install jet build engine?
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "Windows detected, sorry. You're on your own. Cheers!"
+    # Helpz! Anybody want to test out an installation and get this working on Windows?
 fi
 
 glide install
