c193840ca04f68dba423ad923449e7b803ec9d18
Adron Hall <adronhall@gmail.com>
2017-01-17 16:49:19 -0800

    Last few minor changes. with comments.

--- setup.sh
+++ setup.sh
@@ -19,10 +19,12 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # This needs testing... I'll be doing that later today.
     sudo add-apt-repository ppa:masterminds/glide && sudo apt-get update
     sudo apt-get install glide
-    # Install jet build engine?
+    # Install jet build engine? See > https://github.com/Adron/datadiluvium/issues/17
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "Windows detected, sorry. You're on your own. Cheers!"
     # Helpz! Anybody want to test out an installation and get this working on Windows?
+    # Add glide > https://github.com/Adron/datadiluvium/issues/19
+    # Add jet cli > https://github.com/Adron/datadiluvium/issues/18
 fi
 
 glide install
